64cfc2ceeff6c8c5ec5c3a2117d48857294b7b36
ilyasjamil <jamilon33@gmail.com>
2024-04-01 18:07:40 -0500

    fix bug

--- magpie/src/app/profile/page.js
+++ magpie/src/app/profile/page.js
@@ -3,14 +3,16 @@ import { useEffect, useState, useContext } from "react";
 import { UserAuth } from "../context/AuthContext";
 import { AuthContextProvider } from "../context/AuthContext";
 import Profilepage from "../components/Profilepage";
+
+
 const Profile = () => {
-  
+
 
   return (
     <AuthContextProvider>
-    <Profilepage></Profilepage>
+      <Profilepage></Profilepage>
     </AuthContextProvider>
-    
+
   );
 };
 
