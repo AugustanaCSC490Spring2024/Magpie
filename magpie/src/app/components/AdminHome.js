d1c0878b0b3901f9c6ca5793e78cdd300c28b9a9
AymaneSghier <aymanesghier@gmail.com>
2024-04-15 21:00:40 -0500

    Fixed both buttons on the Admin Page

--- magpie/src/app/components/AdminHome.js
+++ magpie/src/app/components/AdminHome.js
@@ -18,19 +18,47 @@ function AdminDashboard() {
     router.push('/adminmessages'); // This will be the route for messaging interface
   };
 
+  
+
+  const buttonStyle = {
+    borderRadius: '30px',
+    padding: '12px 24px',
+    fontSize: '1rem',
+    fontWeight: '600',
+    cursor: 'pointer',
+    outline: 'none',
+    backgroundColor: '#3f51b5', // Set the background color for both buttons
+    color: '#fff', // Set the text color for both buttons
+    '&:hover': {
+      backgroundColor: '#ffeb3b', // Change the background color on hover
+    }
+  };
+  
   return (
     <Container maxWidth="sm">
       <Box textAlign="center" marginTop={10}>
         <Typography variant="h4" gutterBottom>Welcome to Admin Dashboard</Typography>
-        <Button variant="contained" color="primary" onClick={navigateToCustomizeQuestionnaire}>
+        <Button 
+          variant="contained" 
+          color="primary" 
+          onClick={navigateToCustomizeQuestionnaire}
+          sx={buttonStyle}
+          style={{ marginRight: '10px' }} 
+        >
           Customize Questionnaire
         </Button>
-        <Button variant="contained" color="secondary" onClick={navigateToUserList} sx={{ mt: 2 }}>
+        <Button 
+          variant="contained" 
+          color="primary" // Change color to "primary"
+          onClick={navigateToUserList} 
+          sx={buttonStyle}
+        >
           All Users
         </Button>
       </Box>
     </Container>
   );
+  
 }
 
 export default AdminDashboard;
