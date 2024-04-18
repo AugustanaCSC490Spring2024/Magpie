1590603b760c8993500a916dd325690cb451d328
AymaneSghier <aymanesghier@gmail.com>
2024-04-17 21:48:13 -0500

    designed the dashboard ui

--- magpie/src/app/components/Dashboardpage.js
+++ magpie/src/app/components/Dashboardpage.js
@@ -5,6 +5,10 @@ import { useRouter } from "next/navigation";
 import ContactButton from "./Contactbutton";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getMatchingScores } from './matching';
+import { motion } from 'framer-motion';
+
+
+
 
 const DashboardPage = (props) => {
   const { user, logOut, isAdmin } = UserAuth();
@@ -12,7 +16,22 @@ const DashboardPage = (props) => {
   const [users, setUsers] = useState([]);
   const [matchingScores, setMatchingScores] = useState({});
   const [isMounted, setIsMounted] = useState(false); // Flag to check component mount status
-
+  
+    const cardVariants = {
+    offscreen: {
+      y: 50,
+      opacity: 0
+    },
+    onscreen: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8
+      }
+    }
+  };
   useEffect(() => {
     setIsMounted(true); // Set mounted flag
     return () => setIsMounted(false); // Cleanup function to set it to false when unmounted
@@ -64,43 +83,37 @@ const DashboardPage = (props) => {
 
   return (
     <Container maxWidth="xl">
-      <Grid container spacing={2} style={{ paddingLeft: 140, paddingRight: 10, paddingBottom: 80, paddingTop: 30 }}>
-        <Grid item xs={10}>
-          {user && <Typography variant="h6">Welcome <span style={{ fontFamily: 'Arial' }}>{user.displayName}</span></Typography>}
+      <Grid container spacing={2} style={{ padding: '30px 10px 80px 140px' }}>
+        <Grid item xs={12}>
+          {user && <Typography variant="h6">Welcome, {user.displayName}</Typography>}
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={12} style={{ display: 'flex', justifyContent: 'flex-end' }}>
           <Button href='../profile'>Profile</Button>
-        </Grid>
-        <Grid item xs={1}>
           <Button onClick={() => { logOut(); router.push('/#'); }}>Sign Out</Button>
         </Grid>
         {users.map((userProfile, index) => (
           <Grid item xs={4} key={userProfile.id}>
-            <Card style={{ textAlign: 'center', padding: '2.4rem', width: '15rem', height: 'auto', borderRadius: '15px' }}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sx={{ margin: 'auto' }}>
-                  <img src={userProfile.imageUrl || `https://via.placeholder.com/150x150.png?text=No+Image`} style={{ width: '150px', height: '150px', borderRadius: '15px' }} alt={`User ${userProfile.name || "Name not available"}`}></img>
-                </Grid>
-                <Grid item xs={12} sx={{ margin: 'auto' }}>
-                  <Typography variant={'h4'}>{userProfile.name || "Name not available"}</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography>{userProfile.bio || "Bio not available"}</Typography>
-                </Grid>
-                <Grid item xs={12} sx={{ margin: 'auto' }}>
-                  <Typography>{userProfile.email || "Email not available"}</Typography>
-                </Grid>
-                <Grid item xs={12} sx={{ margin: 'auto' }}>
-                  <Typography>Match: {matchingScores[userProfile.id] ? `${matchingScores[userProfile.id].toFixed(1)}%` : "Calculating..."}</Typography>
-                </Grid>
-              </Grid>
-            </Card>
+            <motion.div
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: true, amount: 0.8 }}
+              variants={cardVariants}
+            >
+              <Card style={{ textAlign: 'center', padding: '2.4rem', width: '15rem', borderRadius: '15px' }}>
+                <img src={userProfile.imageUrl || `https://via.placeholder.com/150x150.png?text=No+Image`} alt={`User ${userProfile.name}`} style={{ width: '150px', height: '150px', borderRadius: '15px', margin: 'auto' }} />
+                <Typography variant="h4">{userProfile.name || "Name not available"}</Typography>
+                <Typography>{userProfile.bio || "Bio not available"}</Typography>
+                <Typography>{userProfile.email || "Email not available"}</Typography>
+                <Typography>Match: {matchingScores[userProfile.id] ? `${matchingScores[userProfile.id].toFixed(1)}%` : "Calculating..."}</Typography>
+              </Card>
+            </motion.div>
           </Grid>
         ))}
       </Grid>
     </Container>
   );
-}
+};
+
 
 
 export default DashboardPage;
