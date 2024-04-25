1d625fdf70c17b18c4b12faa71368853aef87dec
aaron <aaronyo2002@gmail.com>
2024-04-25 13:23:44 -0500

    font fix for about page

--- magpie/src/app/components/About.js
+++ magpie/src/app/components/About.js
@@ -6,6 +6,7 @@ import catImage2 from '../../../public/cat2.jpg';
 import './../styles/Dropdown.css';
 import roomate1 from '../../../public/Roomate1.jpg';
 import roomate2 from '../../../public/Roomate2.jpg';
+import '../styles/styles.css'
 
 
 
@@ -19,7 +20,7 @@ export const Header = ({ title }) => (
 export const AboutSection = () => (
     <div className="section">
       <div style={{color: 'white', display: 'flex', alignItems: 'center' }}>
-        <div style={{ marginLeft: '10%' ,  marginRight: '10%' }}>
+        <div className='image-container' style={{ marginLeft: '10%' ,  marginRight: '10%' }}>
           <Image src={roomate1} 
           placeholder='blur'
           quality={100} 
@@ -28,21 +29,21 @@ export const AboutSection = () => (
           height={350}
           alt="roomate1" />
         </div>
-        <div style={{ marginLeft: '10%', marginRight: '10%'}}>
+        <div style={{paddingBottom:  '5%', marginLeft: '1%', marginRight: '5%'}}>
           <p>
           Roommixer revolutionizes college roommate selection by connecting you with compatible roommates based on shared interests and preferences. No more random assignments or compatibility issues – just seamless connections for a harmonious college experience. Join Roommixer today and find your perfect roommate match effortlessly!
           </p>
         </div>
       </div>
-      <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
-        <div style={{marginLeft: '10%', marginRight: '10%'}}>
+      <div style={{ paddingBottom:  '5%',display: 'flex', alignItems: 'center', marginTop: '20px' }}>
+        <div style={{marginLeft: '5%', marginRight: '1%'}}>
           <p>
             
 At Roommixer, we're not just about finding roommates – we're about fostering a community where every student feels supported and understood. Our mission is to create inclusive living environments where diversity is celebrated and friendships thrive. Join us in building a brighter, more connected college experience where everyone belongs.
           </p>
         </div>
-        <div style={{borderRadius: '5%', marginLeft: '10%' , marginRight: '10%'}}>
-          <Image
+        <div className='image-container' style={{borderRadius: '5%', marginLeft: '10%' , marginRight: '10%'}}>
+          <Image 
           src= {roomate2} 
           style={{ borderRadius: '15px' }}
           width={700}
@@ -52,9 +53,9 @@ At Roommixer, we're not just about finding roommates – we're about fostering a
           alt="roomate2" />
         </div>
       </div>
-      <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
-        <div style={{  marginLeft: '10%' , marginRight: '10%' }}>
-        <Image
+      <div style={{ paddingBottom:  '5%', display: 'flex', alignItems: 'center', marginTop: '20px' }}>
+        <div className='image-container' style={{  marginLeft: '10%' , marginRight: '10%' }}>
+        <Image 
           src= {roomate2} 
           style={{ borderRadius: '15px' }}
           width={700}
@@ -63,7 +64,7 @@ At Roommixer, we're not just about finding roommates – we're about fostering a
           quality={100} 
           alt="roomate2" />
         </div>
-        <div style={{marginLeft: '10%', marginRight: '10%'}}>
+        <div style={{marginLeft: '1%', marginRight: '5%'}}>
           <p>
             
 Looking ahead, our aspirations extend beyond just matching roommates. We envision Roommixer as a platform that empowers students to connect, collaborate, and grow together. We're committed to expanding our services to offer resources for academic support, career development, and personal growth. By continuously innovating and listening to our users, we aim to be the go-to destination for all aspects of the college experience, helping students make the most of their journey both inside and outside the classroom.
@@ -100,14 +101,3 @@ export const GettingStartedSection = () => (
   </div>
   
 );
-
-// export const Dropdown = ({ options }) => (
-//     <div className="dropdown">
-//       <select>
-//         {options.map(option => (
-//           <option key={option.value} value={option.value}>{option.label}</option>
-//         ))}
-//       </select>
-//     </div>
-//   );
-  
